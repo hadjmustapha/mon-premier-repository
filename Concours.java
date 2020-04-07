e415c420cc0d5fee5dc6c7eeec26e972d4670989
Lahneche31000 <mousse-29k@hotmail.fr>
2020-04-07 23:27:23 +0100

    mon commit avant chekout

--- Concours.java
+++ Concours.java
@@ -26,8 +26,8 @@ public class Concours {
     public static void main(String[] args) {
         System.out.println("Hello world !");
         System.out.println("Hello world 2!");
-        System.out.println("Hello world 3!");
-        System.out.println("Hello world 4!");
+        System.out.println("Hello world 3! Hello world 3!");
+        
         String  line=null;
         int nb;
         String Tab[];
